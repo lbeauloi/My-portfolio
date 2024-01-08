45f5200e1b23ef2ee149f78eeb82cca007e13baa
Lucas Beauloi <l.beauloi@gmail.com>
2024-01-08 15:18:45 +0100

    ok content in dev

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -43,20 +43,80 @@ const Home = () => {
 
 
         <section class="container" id='projects' ref={slider}>
-            <div class="description panel blue">
-              <h3>projet 1</h3>
+            <div class="panel">
+              <div className='projectContainer'>
+                <div className='projectImg projectImg1'>
+                </div>
+
+                <div className='projectdetails'>
+                    <h2>The Collection</h2>
+                    <p className='description'>A collection of my favorite artists. The cards are dynamically generated in JavaScript. The concepts of DOM, basic HTML CSS JS and responsive design are used in this project.</p>
+                    <div className='langageTag'>
+                        <p>html/css</p>
+                        <p>js</p>
+                        <p>dom</p>
+                        <p>responsive</p>
+                    </div>
+                    <a href="https://lbeauloi.github.io/The-Collection/" target="_blank" className='projectLink'>See the project</a>
+                </div>
+              </div>
             </div>
             
-            <div class="panel red">
-              <h3>projet 2</h3>
+            <div class="panel">
+              <div className='projectContainer'>
+                <div className='projectImg projectImg2'>
+                </div>
+
+              <div className='projectdetails'>
+                  <h2>Weather App</h2>
+                  <p className='description'>A collection of my favorite artists. The cards are dynamically generated in JavaScript. The concepts of DOM, basic HTML CSS JS and responsive design are used in this project.</p>
+                  <div className='langageTag'>
+                      <p>html/css</p>
+                      <p>js</p>
+                      <p>dom</p>
+                      <p>responsive</p>
+                  </div>
+                  <a href="https://lbeauloi.github.io/Weather-app/" target="_blank" className='projectLink'>See the project</a>
+              </div>
+            </div>
             </div>
             
-            <div class="panel orange">
-              <h3>projet 3</h3>
+            <div class="panel">
+              <div className='projectContainer'>
+                  <div className='projectImg projectImg3'>
+                  </div>
+
+                  <div className='projectdetails'>
+                      <h2>Sunnyside Agency</h2>
+                      <p className='description'>A collection of my favorite artists. The cards are dynamically generated in JavaScript. The concepts of DOM, basic HTML CSS JS and responsive design are used in this project.</p>
+                      <div className='langageTag'>
+                          <p>html/css</p>
+                          <p>js</p>
+                          <p>dom</p>
+                          <p>responsive</p>
+                      </div>
+                      <a href="https://lbeauloi.github.io/Sunnyside/" target="_blank" className='projectLink'>See the project</a>
+                  </div>
+                </div>
             </div>
             
-            <div class="panel purple">
-              <h3>projet 4</h3>
+            <div class="panel">
+              <div className='projectContainer'>
+                  <div className='projectImg projectImg4'>
+                  </div>
+
+                  <div className='projectdetails'>
+                      <h2>Movie Browser</h2>
+                      <p className='description'>A collection of my favorite artists. The cards are dynamically generated in JavaScript. The concepts of DOM, basic HTML CSS JS and responsive design are used in this project.</p>
+                      <div className='langageTag'>
+                          <p>html/css</p>
+                          <p>js</p>
+                          <p>dom</p>
+                          <p>responsive</p>
+                      </div>
+                      <a href="https://lbeauloi.github.io/The-Collection/" target="_blank" className='projectLink'>See the project</a>
+                  </div>
+                </div>
             </div>
         </section>
 
@@ -77,80 +137,3 @@ const Home = () => {
 }
 export default Home;
 
-{/* <section className="projectHoriz">
-<section className="Projet1" id="projects">
-  <div className='projectContainer' id="projects">
-    <div className='projectImg projectImg1'>
-    </div>
-
-    <div className='projectdetails'>
-        <h2>The Collection</h2>
-        <p className='description'>A collection of my favorite artists. The cards are dynamically generated in JavaScript. The concepts of DOM, basic HTML CSS JS and responsive design are used in this project.</p>
-        <div className='langageTag'>
-            <p>html/css</p>
-            <p>js</p>
-            <p>dom</p>
-            <p>responsive</p>
-        </div>
-        <a href="https://lbeauloi.github.io/The-Collection/" target="_blank" className='projectLink'>See the project</a>
-        </div>
-</div>
-</section>
-
-<section className="Projet2">
-<div className='projectContainer'>
-  <div className='projectImg projectImg2'>
-  </div>
-
-  <div className='projectdetails'>
-      <h2>Weather App</h2>
-      <p className='description'>A collection of my favorite artists. The cards are dynamically generated in JavaScript. The concepts of DOM, basic HTML CSS JS and responsive design are used in this project.</p>
-      <div className='langageTag'>
-          <p>html/css</p>
-          <p>js</p>
-          <p>dom</p>
-          <p>responsive</p>
-      </div>
-      <a href="https://lbeauloi.github.io/Weather-app/" target="_blank" className='projectLink'>See the project</a>
-  </div>
-</div>
-</section>
-
-<section className="Projet3">
-<div className='projectContainer'>
-  <div className='projectImg projectImg3'>
-  </div>
-
-  <div className='projectdetails'>
-      <h2>Sunnyside Agency</h2>
-      <p className='description'>A collection of my favorite artists. The cards are dynamically generated in JavaScript. The concepts of DOM, basic HTML CSS JS and responsive design are used in this project.</p>
-      <div className='langageTag'>
-          <p>html/css</p>
-          <p>js</p>
-          <p>dom</p>
-          <p>responsive</p>
-      </div>
-      <a href="https://lbeauloi.github.io/Sunnyside/" target="_blank" className='projectLink'>See the project</a>
-  </div>
-</div>
-</section>
-
-<section className="Projet4">
-<div className='projectContainer'>
-  <div className='projectImg projectImg4'>
-  </div>
-
-  <div className='projectdetails'>
-      <h2>Movie Browser</h2>
-      <p className='description'>A collection of my favorite artists. The cards are dynamically generated in JavaScript. The concepts of DOM, basic HTML CSS JS and responsive design are used in this project.</p>
-      <div className='langageTag'>
-          <p>html/css</p>
-          <p>js</p>
-          <p>dom</p>
-          <p>responsive</p>
-      </div>
-      <a href="https://lbeauloi.github.io/The-Collection/" target="_blank" className='projectLink'>See the project</a>
-  </div>
-</div>
-</section>
-</section> */}
