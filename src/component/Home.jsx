9b2547491bbe1bcf18468269ac13d70006505fbd
Lucas Beauloi <l.beauloi@gmail.com>
2024-01-08 14:39:22 +0100

    horiz scroll ok

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,17 +1,38 @@
 import React from 'react';
 import ScrollDownArrow from "./ScrollDownArrow";
 import BackToTop from './BackToTop';
-
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import { useLayoutEffect, useRef } from "react";
 import "/assets/css/Home.css";
 import '/assets/css/Contact.css';
 
-
+gsap.registerPlugin(ScrollTrigger);
 const Home = () => {
-
+  const component = useRef();
+  const slider = useRef();
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      let panels = gsap.utils.toArray(".panel");
+      gsap.to(panels, {
+        xPercent: -100 * (panels.length - 1),
+        ease: "none",
+        scrollTrigger: {
+          trigger: slider.current,
+          pin: true,
+          scrub: 1,
+          snap: 1 / (panels.length - 1),
+          end: () => "+=" + slider.current.offsetWidth,
+          markers: true,
+        },
+      });
+    }, component);
+    return () => ctx.revert();
+  });
     return(
       <>
         <BackToTop/>
-
+      <div className="App" ref={component}>
         <section className="Home">
           <p className="line1" id="hello"><em>hello, i'm</em></p>
           <img className="line2" id="LB" src="/assets/img/lucasbeauloi.svg"></img>
@@ -21,25 +42,22 @@ const Home = () => {
         </section>    
 
 
-        <section class="horizontal-scroll">
-          <div class="horizontal-scroll__inner">
-            
-            <div class="horizontal-text">
+        <section class="container" id='projects' ref={slider}>
+            <div class="description panel blue">
               <h3>projet 1</h3>
             </div>
             
-            <div class="horizontal-text">
+            <div class="panel red">
               <h3>projet 2</h3>
             </div>
             
-            <div class="horizontal-text">
+            <div class="panel orange">
               <h3>projet 3</h3>
             </div>
             
-            <div class="horizontal-text">
+            <div class="panel purple">
               <h3>projet 4</h3>
             </div>
-          </div>
         </section>
 
         <section className='contactContainer'id='contact'>
@@ -52,6 +70,7 @@ const Home = () => {
             </div>
             <p className='line4'>Coded by <a href="https://www.linkedin.com/in/lucas-beauloi/" target='_blank'><strong>Lucas Beauloi</strong></a>, designed by <a href="http://laurakeerman.be/" target='_blank'><strong>Laura Keerman</strong></a></p>
         </section>
+      </div>
       </>
         
     )
