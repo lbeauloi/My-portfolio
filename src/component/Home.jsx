0961d89508060564f2b4d7ba702f88c3c90c2829
Lucas Beauloi <l.beauloi@gmail.com>
2024-01-09 09:51:03 +0100

    img fix

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -37,7 +37,7 @@ const Home = () => {
         
         <section className="Home">
           <p className="line1" id="hello"><em>hello, i'm</em></p>
-          <h1 className="line2" id="LB"><img srcset="/assets/img/lucasbeauloi.svg"></img></h1>
+          <h1 className="line2" id="LB"><img src="/assets/img/lucasbeauloi.svg" alt="Lucas Beauloi" /></h1>
           <p className="line3">a junior front-end developer</p>
           <p className="line4">Currently undergoing front-end training at BeCode, I am eager to apply my skills and actively seeking a challenging internship opportunity. Let's build something remarkable together!</p>
           <div className="line5"><ScrollDownArrow/></div>
