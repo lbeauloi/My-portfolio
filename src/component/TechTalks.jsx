666744149dc246777d37b773d283eae4dba0f304
Lucas Beauloi <l.beauloi@gmail.com>
2024-01-24 12:23:57 +0100

    fixing pdf files maybe

--- src/component/TechTalks.jsx
+++ src/component/TechTalks.jsx
@@ -6,17 +6,11 @@ const TechTalks = () => {
       <p className='TechDescription'>During my training at BeCode, I had the opportunity to present several "tech talks." The goal of these brief presentations, lasting about ten minutes each, is to promote awareness and encourage technological exploration. <br /> Here are the 5 tech talks that I have presented, happy reading!
       </p>
       <div className='TechTalkDiv'>
-            <a href="/assets/TechTalks/Tech_Talk_1_Images_Format.pdf" target='_blank'><div className='Tech1'>Open</div></a>
-        
-            <a href="/assets/TechTalks/Tech_Talk_2_UI-UX_design.pdf" target='_blank'><div className='Tech2'>Open</div></a>
-
-            <a href="/assets/TechTalks/Tech_Talk_3_WordPress.pdf" target='_blank'><div className='Tech3'>Open</div></a>
-
-            <a href="/assets/TechTalks/Tech_Talk_4_GSAP.pdf" target='_blank'><div className='Tech4'>Open</div></a>
-
-        <div className='Tech5'>
-            <a href="" target='_blank'>Open</a>
-        </div>
+        <div className='Tech1' onClick={() => window.open("/assets/TechTalks/Tech_Talk_1_Images_Format.pdf", "_blank")}>Open</div>
+        <div className='Tech2' onClick={() => window.open("/assets/TechTalks/Tech_Talk_2_UI-UX_design.pdf", "_blank")}>Open</div>
+        <div className='Tech3' onClick={() => window.open("/assets/TechTalks/Tech_Talk_3_WordPress.pdf", "_blank")}>Open</div>
+        <div className='Tech4' onClick={() => window.open("/assets/TechTalks/Tech_Talk_4_GSAP.pdf", "_blank")}>Open</div>
+        <div className='Tech5' onClick={() => window.open("", "_blank")}>Open</div>
       </div>
     </div>
   )
