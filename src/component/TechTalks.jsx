59a0e21c7166f3f0d52693e8160862ad0530bd56
Lucas Beauloi <l.beauloi@gmail.com>
2024-01-24 13:18:41 +0100

    try to fix pdf files open

--- src/component/TechTalks.jsx
+++ src/component/TechTalks.jsx
@@ -1,4 +1,9 @@
 import '/assets/css/TechTalks.css';
+import Tech1 from '/assets/TechTalks/Tech_Talk_1_Images_Format.pdf';
+import Tech2 from '/assets/TechTalks/Tech_Talk_2_UI-UX_design.pdf';
+import Tech3 from '/assets/TechTalks/Tech_Talk_3_WordPress.pdf';
+import Tech4 from '/assets/TechTalks/Tech_Talk_4_GSAP.pdf';
+
 const TechTalks = () => {
   return (
     <div className="TechTalkContainer">
@@ -6,11 +11,16 @@ const TechTalks = () => {
       <p className='TechDescription'>During my training at BeCode, I had the opportunity to present several "tech talks." The goal of these brief presentations, lasting about ten minutes each, is to promote awareness and encourage technological exploration. <br /> Here are the 5 tech talks that I have presented, happy reading!
       </p>
       <div className='TechTalkDiv'>
-        <div className='Tech1' onClick={() => window.open("/assets/TechTalks/Tech_Talk_1_Images_Format.pdf", "_blank")}>Open</div>
-        <div className='Tech2' onClick={() => window.open("/assets/TechTalks/Tech_Talk_2_UI-UX_design.pdf", "_blank")}>Open</div>
-        <div className='Tech3' onClick={() => window.open("/assets/TechTalks/Tech_Talk_3_WordPress.pdf", "_blank")}>Open</div>
-        <div className='Tech4' onClick={() => window.open("/assets/TechTalks/Tech_Talk_4_GSAP.pdf", "_blank")}>Open</div>
-        <div className='Tech5' onClick={() => window.open("", "_blank")}>Open</div>
+            <a href={Tech1} without rel="noopener noreferrer" target='_blank'><div className='Tech1'>Open</div></a>
+        
+            <a href={Tech2} target='_blank'><div className='Tech2'>Open</div></a>
+
+            <a href={Tech3} target='_blank'><div className='Tech3'>Open</div></a>
+
+            <a href={Tech4} target='_blank'><div className='Tech4'>Open</div></a>
+
+            <a href="" target='_blank'><div className='Tech5'>Open</div></a>
+  
       </div>
     </div>
   )
